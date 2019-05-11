451b8d178cb73bc1f72df5007d4247074752c5e5
michaelcohen716 <michaelcohen716@gmail.com>
2019-05-11 13:01:07 -0400

    add contract building blocks

---
+++ contracts/Decentralize.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.5.0;
+
+contract Decentralize {
+  uint projectId = 1;
+
+  mapping(uint => bytes32) projectIdToName;
+  mapping(uint => address) projectIdToOwner;
+  mapping(uint => bytes32) projectIdToIPFSHash;
+  mapping(uint => bytes32[]) projectIdToCommits;
+
+  mapping(uint => mapping(address => bool)) projectPermissions; // projectId => (userAddress => bool) ...does user have permission?
+
+  event Commit(uint indexed _projectId, address _user, bytes32 _message);
+
+  modifier onlyOwner(uint _projectId){
+    require(projectIdToOwner[_projectId] == msg.sender, "Sorry, only project owner can send invitations.");
+  }
+
+  modifier onlyPermissionedUsers(uint _projectId){
+    require(projectPermissions[_projectId][msg.sender] == true, "Sorry, only permissioned users can submit code");
+  }
+
+  function createProject(bytes32 _name) public {
+    projectIdToName[projectId] = _name;
+    projectPermissions[projectId][msg.sender] = true;
+    projectIdToOwner[projectId++] = msg.sender;
+  }
+
+  function inviteTeammate(uint _projectId, address _teammate) public onlyOwner(_projectId){
+    projectPermissions[_projectId][_teammate] = true;
+  }
+
+  // updates projectId => IPFS hash pointer
+  function submitCode(uint _projectId, bytes32 _newHash, bytes32 _commitMessage) public onlyPermissionedUsers(_projectId){
+    projectIdToIPFSHash[_projectId] = _newHash;
+    emit Event(_projectId, msg.sender, _commitMessage)
+  }
+
+
+}
